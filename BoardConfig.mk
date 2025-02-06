a1ed26a5a7cb550ffeaaac2ea4f098a7f9da0cc3
Frost <159105703+Karan-Frost@users.noreply.github.com>
2025-02-06 13:48:36 +0000

    miatoll: Add support for Dolby Atmos
    Co-authored-by: Sugakesshaa <sugakesshaa@pixelos.net>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Change-Id: I6c553e102d2756fc0ff9ac1d8fbeb6e336564123
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml \
+    vendor/oneplus/dolby/vintf/dolby_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += vendor/oneplus/dolby/vintf/dolby_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
